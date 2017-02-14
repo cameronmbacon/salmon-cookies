df61906cc92650434999770ff156a680479469b3
Cameron Bacon <cameronmbacon@gmail.com>
2017-02-13 19:35:08 -0800

    app.js renders results properly.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
 
-/* This is an application for a fake business called Salmon Cookies
+/*
+ * This is an application for a fake business called Salmon Cookies
  * Created By: Cameron Bacon on 02/13/2017
  * Last Modified: 02/13/2017
 */
 
-//1st and Pike location
+// ALL LOCATIONS REPRESENTED AS OBJECT LITERALS
+
+//First and Pike location
 var firstAndPike = {
+  name: 'First and Pike',
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   avgCookiesPerCustomer: 6.3,
@@ -17,7 +21,7 @@ var firstAndPike = {
     var total = 0;
     for (var i = 0; i < this.hoursOpenPerDay; i++) {
       var randNumCustomers = this.genRandNumCustomers();
-      simSales = Math.floor(this.avgCookiesPerCustomer * randNumCustomers);
+      simSales = Math.ceil(this.avgCookiesPerCustomer * randNumCustomers);
       this.simulatedSales.push(simSales);
       total += simSales;
     }
@@ -25,11 +29,33 @@ var firstAndPike = {
   },
   genRandNumCustomers: function() {
     return Math.floor((Math.random() * this.maxHourlyCustomers) + this.minHourlyCustomers);
+  },
+  renderToPage: function() {
+    var headingEl = document.createElement('h1');
+    headingEl.setAttribute('class', 'store-name'); // headingEl = <h1 class="store-name"></h1>
+    var articleEl = document.createElement('article');
+    articleEl.setAttribute('class', 'store'); // articleEl = <article class="store"></article>
+    var unorderedEl = document.createElement('ul');
+    unorderedEl.setAttribute('class', 'list'); // unorderedEl = <ul class="list"></ul>
+    var sectionEl = document.getElementById('main-content'); //parent container
+
+    headingEl.textContent = this.name; // populates heading with store name
+    articleEl.appendChild(headingEl);
+
+    for (var i = 0; i < this.simulatedSales.length; i++) {
+      var listItemEl = document.createElement('li');
+      listItemEl.setAttribute('class', 'list-item'); // listItemEl = <li class="list-item"></li>
+      listItemEl.textContent = this.simulatedSales[i]; // listItemEl = <li class="list-item">content passed from array</li>
+      unorderedEl.appendChild(listItemEl); // adds list items to list
+    }
+    articleEl.appendChild(unorderedEl); // adds list to article
+    sectionEl.appendChild(articleEl); // adds article to <section id="main-content"></section>
   }
 };
 
 //SeaTac Airport location
 var seaTacAirport = {
+  name: 'SeaTac Airport',
   minHourlyCustomers: 3,
   maxHourlyCustomers: 24,
   avgCookiesPerCustomer: 1.2,
@@ -40,7 +66,7 @@ var seaTacAirport = {
     var total = 0;
     for (var i = 0; i < this.hoursOpenPerDay; i++) {
       var randNumCustomers = this.genRandNumCustomers();
-      simSales = Math.floor(this.avgCookiesPerCustomer * randNumCustomers);
+      simSales = Math.ceil(this.avgCookiesPerCustomer * randNumCustomers);
       this.simulatedSales.push(simSales);
       total += simSales;
     }
@@ -48,11 +74,33 @@ var seaTacAirport = {
   },
   genRandNumCustomers: function() {
     return Math.floor((Math.random() * this.maxHourlyCustomers) + this.minHourlyCustomers);
+  },
+  renderToPage: function() {
+    var headingEl = document.createElement('h1');
+    headingEl.setAttribute('class', 'store-name'); // headingEl = <h1 class="store-name"></h1>
+    var articleEl = document.createElement('article');
+    articleEl.setAttribute('class', 'store'); // articleEl = <article class="store"></article>
+    var unorderedEl = document.createElement('ul');
+    unorderedEl.setAttribute('class', 'list'); // unorderedEl = <ul class="list"></ul>
+    var sectionEl = document.getElementById('main-content'); //parent container
+
+    headingEl.textContent = this.name; //
+    articleEl.appendChild(headingEl);
+
+    for (var i = 0; i < this.simulatedSales.length; i++) {
+      var listItemEl = document.createElement('li');
+      listItemEl.setAttribute('class', 'list-item'); // listItemEl = <li class="list-item"></li>
+      listItemEl.textContent = this.simulatedSales[i]; // listItemEl = <li class="list-item">content passed from array</li>
+      unorderedEl.appendChild(listItemEl); // adds list items to list
+    }
+    articleEl.appendChild(unorderedEl); // adds list to article
+    sectionEl.appendChild(articleEl); // adds article to <section id="main-content"></section>
   }
 };
 
 //Seattle Center location
 var seattleCenter = {
+  name: 'Seattle Center',
   minHourlyCustomers: 11,
   maxHourlyCustomers: 38,
   avgCookiesPerCustomer: 3.7,
@@ -63,7 +111,7 @@ var seattleCenter = {
     var total = 0;
     for (var i = 0; i < this.hoursOpenPerDay; i++) {
       var randNumCustomers = this.genRandNumCustomers();
-      simSales = Math.floor(this.avgCookiesPerCustomer * randNumCustomers);
+      simSales = Math.ceil(this.avgCookiesPerCustomer * randNumCustomers);
       this.simulatedSales.push(simSales);
       total += simSales;
     }
@@ -71,11 +119,33 @@ var seattleCenter = {
   },
   genRandNumCustomers: function() {
     return Math.floor((Math.random() * this.maxHourlyCustomers) + this.minHourlyCustomers);
+  },
+  renderToPage: function() {
+    var headingEl = document.createElement('h1');
+    headingEl.setAttribute('class', 'store-name'); // headingEl = <h1 class="store-name"></h1>
+    var articleEl = document.createElement('article');
+    articleEl.setAttribute('class', 'store'); // articleEl = <article class="store"></article>
+    var unorderedEl = document.createElement('ul');
+    unorderedEl.setAttribute('class', 'list'); // unorderedEl = <ul class="list"></ul>
+    var sectionEl = document.getElementById('main-content'); //parent container
+
+    headingEl.textContent = this.name; //
+    articleEl.appendChild(headingEl);
+
+    for (var i = 0; i < this.simulatedSales.length; i++) {
+      var listItemEl = document.createElement('li');
+      listItemEl.setAttribute('class', 'list-item'); // listItemEl = <li class="list-item"></li>
+      listItemEl.textContent = this.simulatedSales[i]; // listItemEl = <li class="list-item">content passed from array</li>
+      unorderedEl.appendChild(listItemEl); // adds list items to list
+    }
+    articleEl.appendChild(unorderedEl); // adds list to article
+    sectionEl.appendChild(articleEl); // adds article to <section id="main-content"></section>
   }
 };
 
 //Capitol Hill location
 var capitolHill = {
+  name: 'Capitol Hill',
   minHourlyCustomers: 20,
   maxHourlyCustomers: 38,
   avgCookiesPerCustomer: 2.3,
@@ -86,7 +156,7 @@ var capitolHill = {
     var total = 0;
     for (var i = 0; i < this.hoursOpenPerDay; i++) {
       var randNumCustomers = this.genRandNumCustomers();
-      simSales = Math.floor(this.avgCookiesPerCustomer * randNumCustomers);
+      simSales = Math.ceil(this.avgCookiesPerCustomer * randNumCustomers);
       this.simulatedSales.push(simSales);
       total += simSales;
     }
@@ -94,11 +164,33 @@ var capitolHill = {
   },
   genRandNumCustomers: function() {
     return Math.floor((Math.random() * this.maxHourlyCustomers) + this.minHourlyCustomers);
+  },
+  renderToPage: function() {
+    var headingEl = document.createElement('h1');
+    headingEl.setAttribute('class', 'store-name'); // headingEl = <h1 class="store-name"></h1>
+    var articleEl = document.createElement('article');
+    articleEl.setAttribute('class', 'store'); // articleEl = <article class="store"></article>
+    var unorderedEl = document.createElement('ul');
+    unorderedEl.setAttribute('class', 'list'); // unorderedEl = <ul class="list"></ul>
+    var sectionEl = document.getElementById('main-content'); //parent container
+
+    headingEl.textContent = this.name; //
+    articleEl.appendChild(headingEl);
+
+    for (var i = 0; i < this.simulatedSales.length; i++) {
+      var listItemEl = document.createElement('li');
+      listItemEl.setAttribute('class', 'list-item'); // listItemEl = <li class="list-item"></li>
+      listItemEl.textContent = this.simulatedSales[i]; // listItemEl = <li class="list-item">content passed from array</li>
+      unorderedEl.appendChild(listItemEl); // adds list items to list
+    }
+    articleEl.appendChild(unorderedEl); // adds list to article
+    sectionEl.appendChild(articleEl); // adds article to <section id="main-content"></section>
   }
 };
 
 //Alki location
 var alki = {
+  name: 'Alki',
   minHourlyCustomers: 2,
   maxHourlyCustomers: 16,
   avgCookiesPerCustomer: 4.6,
@@ -109,7 +201,7 @@ var alki = {
     var total = 0;
     for (var i = 0; i < this.hoursOpenPerDay; i++) {
       var randNumCustomers = this.genRandNumCustomers();
-      simSales = Math.floor(this.avgCookiesPerCustomer * randNumCustomers);
+      simSales = Math.ceil(this.avgCookiesPerCustomer * randNumCustomers);
       this.simulatedSales.push(simSales);
       total += simSales;
     }
@@ -117,37 +209,49 @@ var alki = {
   },
   genRandNumCustomers: function() {
     return Math.floor((Math.random() * this.maxHourlyCustomers) + this.minHourlyCustomers);
+  },
+  renderToPage: function() {
+    var headingEl = document.createElement('h1');
+    headingEl.setAttribute('class', 'store-name'); // headingEl = <h1 class="store-name"></h1>
+    var articleEl = document.createElement('article');
+    articleEl.setAttribute('class', 'store'); // articleEl = <article class="store"></article>
+    var unorderedEl = document.createElement('ul');
+    unorderedEl.setAttribute('class', 'list'); // unorderedEl = <ul class="list"></ul>
+    var sectionEl = document.getElementById('main-content'); //parent container
+
+    headingEl.textContent = this.name; //
+    articleEl.appendChild(headingEl);
+
+    for (var i = 0; i < this.simulatedSales.length; i++) {
+      var listItemEl = document.createElement('li');
+      listItemEl.setAttribute('class', 'list-item'); // listItemEl = <li class="list-item"></li>
+      listItemEl.textContent = this.simulatedSales[i]; // listItemEl = <li class="list-item">content passed from array</li>
+      unorderedEl.appendChild(listItemEl); // adds list items to list
+    }
+    articleEl.appendChild(unorderedEl); // adds list to article
+    sectionEl.appendChild(articleEl); // adds article to <section id="main-content"></section>
   }
 };
 
-// var userFullName = prompt('Please enter your full name');
-// var userEmail = prompt('Please enter your email');
-//
-// var myUser = {
-//   fullName: userFullName,
-//   email: userEmail,
-//   login: function() {
-//     console.log(this.fullName + ' has now logged in!');
-//   }
-// };
-//
-// console.log('-------------NEW USER---------------');
-// console.log(myUser);
-//
-// myUser.login();
-//
-// console.log('-------------PRESENT ELEMENTS---------------');
-//
-// var userElement = document.createElement('h1'); //STEP 1: create element OR access element from DOM.
-// // userElement = <h1></h1>
-//
-// userElement.setAttribute('id', 'first-user-heading'); //STEP 2: assign any attributes and text content.
-// // userElement = <h1 id="first-user-heading"></h1>
-//
-// userElement.textContent = myUser.fullName;
-// // userElement = <h1 id="first-user-heading">User's full name</h1>
-//
-// var sectionEl = document.getElementById('main-content'); //got reference
-// // sectionEl = <section id="main-content"></section> <-- from our index.html
-//
-// sectionEl.appendChild(userElement); //STEP 3:
+// this array contains my objects
+var locations = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
+
+// Function declarations
+function getSimulatedData() {
+  for (var i = 0; i < locations.length; i++) {
+    locations[i].genSimulatedHourlySales();
+  }
+};
+
+function renderAllResults() {
+  for (var i = 0; i < locations.length; i++) {
+    locations[i].renderToPage();
+  }
+};
+
+//START OF APPLICATION
+
+getSimulatedData();
+renderAllResults();
+
+// END OF APPLICATION
