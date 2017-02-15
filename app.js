4439f3651fb2f612b4b040c9111274cd7bfa1ae5
Cameron Bacon <cameronmbacon@gmail.com>
2017-02-15 09:05:01 -0800

    added more comments for understanding

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 /*
  * This is an application for a fake business called Salmon Cookies
  * Created By: Cameron Bacon on 02/13/2017
- * Last Modified: 02/14/2017
+ * Last Modified: 02/15/2017
 */
 
 // ----------DECLARE AND INITIALIZE VARIABLES----------
@@ -54,7 +54,7 @@ CookieStore.prototype.renderToTable = function() {
 // ------------FUNCTION DECLARATIONS------------
 //This function renders the table header to the sales page
 function generateTableHeader() {
-  var tHCount = 17;  //there are 16 <th></th> elements to be created after the blank ==> stops when i <= tHCount
+  var tHCount = 17;  //there are 16 <th></th> elements to be created after the blank
   var tableHeaderEl = document.createElement('thead'); //<thead></thead>
   var rowEl = document.createElement('tr'); //<tr></tr>
   for (var i = 0; i <= tHCount; i++) {
@@ -77,14 +77,14 @@ function generateTableHeader() {
       thEl.textContent = 'Daily Location Total';
       rowEl.appendChild(thEl);
     }
-  }
+  } //end of loop at i <= 17 because final thEl is appended within the loop
   tableHeaderEl.appendChild(rowEl);
   kalEl.appendChild(tableHeaderEl);
 };
 
 //This function renders the table footer to the sales page
 function generateTableFooter() {
-  var tHCount = 17;
+  var tHCount = 17; //total of 17 td's and th's to be made in table footer
   var tableFooterEl = document.createElement('tfoot'); //<tfoot></tfoot>
   var rowEl = document.createElement('tr'); //<tr></tr>
   var hourlyTotal = 0;
@@ -109,7 +109,7 @@ function generateTableFooter() {
   kalEl.appendChild(tableFooterEl);
 };
 
-// Creating objects using constructor function
+// Creating objects for each location using constructor function
 var pikePlace = new CookieStore('Pike Place', 23, 65, 6.3);
 var seaTacAirport = new CookieStore('Seatac Airport', 3, 24, 1.2);
 var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
@@ -122,14 +122,15 @@ var stores = [pikePlace, seaTacAirport, seattleCenter, capitolHill, alki];
 // ------------START APPLICATION------------
 for (var i = 0; i < stores.length; i++) {
   stores[i].generateSimulatedHourlySales();
-}
+  console.log(stores[i].simulatedSales);
+} //ensures arrays are populated with new data before rendering the table
 
 generateTableHeader();
 
 for (var i = 0; i < stores.length; i++) {
   stores[i].renderToTable();
-}
+} //renders table row for each object in my stores array
 
-generateTableFooter();
+generateTableFooter(); //renders the hourly totals and grand total to the table footer
 
 jorEl.appendChild(kalEl);
